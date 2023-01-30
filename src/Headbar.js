c90ff2c6fd26d68c22d2b39da496148228e25e35
Oladunni Waters <waters.oladunni@gmail.com>
2023-01-30 23:27:12 +0100

    dashboard version 2

--- src/Headbar.js
+++ src/Headbar.js
@@ -1,41 +1,15 @@
-import React  from "react";
-import PropTypes from "prop-types";
-import AppBar from "@material-ui/core/AppBar";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Divider from "@material-ui/core/Divider";
-import Drawer from "@material-ui/core/Drawer";
-import Hidden from "@material-ui/core/Hidden";
-import IconButton from "@material-ui/core/IconButton";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import MenuIcon from "@mui/icons-material/Menu";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import ListItemButton from '@mui/material/ListItemButton';
-
-import {
-  Apps,
-  ContactMail,
-  AssignmentInd,
-  Home
-} from "@mui/icons-material";
-
-import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
-
-import Starred from './Starred';
-import Inbox from './Inbox'
-
-
-/****************** start *****************/
+import * as React from 'react';
 import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
 import Badge from '@mui/material/Badge';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import MailIcon from '@mui/icons-material/Mail';
@@ -84,59 +58,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-/****************** End *****************/
-
-
-
-
-const drawerWidth = 240;
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex"
-  },
-  drawer: {
-    [theme.breakpoints.up("sm")]: {
-      width: drawerWidth,
-      flexShrink: 0
-    }
-  },
-  appBar: {
-    marginLeft: drawerWidth,
-    [theme.breakpoints.up("sm")]: {
-      width: `calc(100% - ${drawerWidth}px)`
-    }
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up("sm")]: {
-      display: "none"
-    }
-  },
-  toolbar: theme.mixins.toolbar,
-  drawerPaper: {
-    width: drawerWidth
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3)
-  },
-  iconStyle: {
-   color: '#A9A9A9' ,
-   width: '20px',
-   height: '20px',
-   "&:hover, &.Mui-focusVisible": { color: "#2752E7" }   
-  },
-  headBar: {
-          [theme.breakpoints.down("md")]: {
-      display: "none"
-    } ,
-  }
-}));
-
-function Sidebar(props) {
-  /****************** Start*****************/
-
+export default function PrimarySearchAppBar() {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
@@ -234,72 +156,17 @@ function Sidebar(props) {
       </MenuItem>
     </Menu>
   );
-  
-  /****************** End *****************/
-  
-  const listItems = [
-  {
-    listIcon: <Home />,
-    listText: "Inbox"
-  },
-  {
-    listIcon: <AssignmentInd />,
-    listText: "Starred"
-  },
-  {
-    listIcon: <Apps />,
-    listText: "Portfolio"
-  },
-  {
-    listIcon: <ContactMail />,
-    listText: "Contacts"
-  }
-];
-  
-  const { container } = props;
-  const classes = useStyles();
-  const theme = useTheme();
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-
-  const drawer = (
-    <div>
-      <div className={classes.toolbar} />
-      <Divider />
-      <List>
-        {listItems.map((listItem, index) => (
-          <ListItem key={listItem} component={Link} to={"/" + listItem.listText}>
-          <ListItemButton   sx={{
-                color: '#A9A9A9',
-                '&:hover': {
-                  color: '#2752E7'
-                },
-              }}>
-            <ListItemIcon className={classes.iconStyle}>
-              {listItem.listIcon}
-            </ListItemIcon>
-            <ListItemText primary={listItem.listText}/>
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
+    <Box sx={{ flexGrow: 1 }}>  
+      <AppBar position="static">
         <Toolbar>
           <IconButton
+            size="large"
+            edge="start"
             color="inherit"
             aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            className={classes.menuButton}
+            sx={{ mr: 2 }}
           >
             <MenuIcon />
           </IconButton>
@@ -307,7 +174,7 @@ function Sidebar(props) {
             variant="h6"
             noWrap
             component="div"
-            className={classes.headBar}
+            sx={{ display: { xs: 'none', sm: 'block' } }}
           >
             MUI
           </Typography>
@@ -362,61 +229,8 @@ function Sidebar(props) {
           </Box>
         </Toolbar>
       </AppBar>
-      <BrowserRouter>
-        <nav className={classes.drawer} aria-label="mailbox folders">
-          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-          <Hidden smUp implementation="css">
-            <Drawer
-              container={container}
-              variant="temporary"
-              anchor={theme.direction === "rtl" ? "right" : "left"}
-              open={mobileOpen}
-              onClose={handleDrawerToggle}
-              classes={{
-                paper: classes.drawerPaper
-              }}
-              ModalProps={{
-                keepMounted: true // Better open performance on mobile.
-              }}
-            >
-              {drawer}
-            </Drawer>
-          </Hidden>
-          <Hidden xsDown implementation="css">
-            <Drawer
-              classes={{
-                paper: classes.drawerPaper
-              }}
-              variant="permanent"
-              open
-            >
-              {drawer}
-            </Drawer>
-          </Hidden>
-        </nav>
-
-        <main className={classes.content}>
-          <div className={classes.toolbar} />
-
-          <Routes>
-            <Route exact path="/" render={() => <div>Home Page</div>} />
-            <Route path="/Inbox" element={<Inbox/>}  />
-            <Route path="/Starred" element={<Starred />} />
-          </Routes>
-        </main>
-      </BrowserRouter>
-    </div>
+      {renderMobileMenu}
+      {renderMenu}
+    </Box>
   );
 }
-
-Sidebar.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  container: PropTypes.instanceOf(
-    typeof Element === "undefined" ? Object : Element
-  )
-};
-
-export default Sidebar;
